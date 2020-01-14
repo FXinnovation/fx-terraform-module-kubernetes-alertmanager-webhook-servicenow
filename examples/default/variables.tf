7339c73d455e65c77d04da4aaf7b6cd00178f66b
Vincent Creusot <vincent.creusot@fxinnovation.com>
2020-01-14 14:16:19 -0600

    Fixed deprecated tf formats

--- examples/default/variables.tf
+++ examples/default/variables.tf
@@ -1,15 +1,15 @@
 variable "client_id" {
-  type = "string"
+  type = string
 }
 
 variable "client_secret" {
-  type = "string"
+  type = string
 }
 
 variable "subscription_id" {
-  type = "string"
+  type = string
 }
 
 variable "tenant_id" {
-  type = "string"
+  type = string
 }
